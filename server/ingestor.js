e33a51375cd45bed74e839353e9536a97bfa4f7f
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2023-11-24 14:30:45 +0530

    Update ingestor.js

--- server/ingestor.js
+++ server/ingestor.js
@@ -10,7 +10,7 @@ const esClient = new Client({ node: 'http://localhost:9200' }); // Update with y
 // set limit of request body size
 app.use(express.json({ limit: '50mb' }));
 
-const index = 'cis_logs';
+const index = 'cisresults';
 
 // Log ingestion endpoint
 app.post('/ingest_single', async (req, res) => {
